7a2b2c0a485647a11b6c5b651a13b17df753b326
Brendan Fortuner <fortuner@amazon.com>
2016-01-25 14:13:49 -0800

    added more projects

--- app/projects/components/projects.ts
+++ app/projects/components/projects.ts
@@ -17,10 +17,8 @@ interface Project {
 var PROJECTS: Project[] = [
   {
     'title': 'BigNeuron',
-    'desc': `Fully-managed cloud service for generating 3D reconstructions of single neurons.
-             Built on top of the open-source 3D imaging library Vaa3D, the tool utilizes
-             Autoscaling and Map Reduce techniques to provide on-demand
-             high performance compute at minimal cost. A collaboration with researchers at 
+    'desc': `Fully-managed cloud service for generating 3D neuron reconstructions using auto-scaling, 
+             MapReduce, and open-source imaging library, Vaa3D. A collaboration with researchers at 
              the Allen Institute for Brain Science in Seattle.`,
     'urls' : [
       {
@@ -45,11 +43,8 @@ var PROJECTS: Project[] = [
   },
   {
     'title': 'Problee',
-    'desc': `Programming practice problems on your phone! 
-            Using drag-and-drop templates, dynamic word banks,
-            and Pandora-style shuffling, Problee delivers an interactive, 
-            mobile learning experience to help you stay sharp without 
-            touching a keyboard.`,
+    'desc': `Learn to program without touching a keyboard! Mobile app designed to make learning 
+             to code easy using drag-and-drop templates, word banks, and Pandora-style shuffling.`,
     'urls' : [
       {
         'name' : 'Website',
@@ -64,31 +59,50 @@ var PROJECTS: Project[] = [
     'created': '2014'
   },
   {
-    'title': 'Jimini',
-    'desc': `Jimini lets users wrap their digital gifts in physical paper 
-             designs they can deliver in person. Browse through 100s of designs, 
-             make online purchase in Amazon or iTunes. Select a design. Jimini 
-             creates a printable paper design with your message and a QR code 
-             the recipient can use to redeem your gift.`,
+    'title': 'Milestone',
+    'desc': `Won 1st place at Angel Hack Seattle building a mobile app "Search Engine For Your 
+             Dream Job." Uses the LinkedIn API to return aggregate statistics about job roles, 
+             universities, degrees, and geographies of employees holding a user's dream job.`,
     'urls' : [
       {
-        'name' : 'Website',
-        'link' : 'https://www.facebook.com/jimini.co'
-      },
+        'name' : 'Github',
+        'link' : 'https://github.com/bfortuner/milestone'
+      }
+    ],
+    'stack': 'Node.js, Express, JQuery',
+    'created': '2014'
+  },
+  {
+    'title': 'Jimini',
+    'desc': `Won audience favorite at Startup Weekend Seattle. Jimini lets users wrap their 
+            digital gifts (mp3s, videos) in origami designs they can deliver in person.`,
+    'urls' : [
       {
         'name' : 'Github',
-        'link' : 'https://github.com/Asimov4/jimini'
+        'link' : 'https://github.com/bfortuner/jimini'
       }
     ],
     'stack': 'Python, Django',
     'created': '2013'
   },
   {
+    'title': 'Eat My Words',
+    'desc': `Won 2nd place at Startup Weekend Seattle making a vocabulary learning game 
+            that kids liked with Unity.`,
+    'urls' : [
+      {
+        'name' : 'Demo',
+        'link' : 'http://thefunstudio.com/eatmywords/Web.html'
+      }
+    ],
+    'stack': 'Unity, Photoshop',
+    'created': '2012'
+  },
+  {
     'title': 'The Yellow Journal',
     'desc': `Relaunched University of Virgina's oldest humor publication, Ye Yellow Journal. 
             Originally founded in 1912 under the slogan "All The News That Is Unfit To Print," the Journal 
-            was discontinued in 1934 when the University's Administrative Council set forth an 
-            order forbidding "the publication or sale of any anonymous paper", citing material 
+            was disbanded in 1934 by the University's Administrative Council citing material 
             they found "scurrilous and indecent in the extreme." Today the Journal is published 
             semesterly and features satirical news articles centered around student life at UVA.`,
     'urls' : [
